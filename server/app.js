a62ff00be8b38cde59bd316e1ccddaa0e5c7bcda
dansoah <fixmyprinter@danilorechi.net>
2017-01-21 00:53:38 -0200

    Add city-info and user-latlong routes

--- server/app.js
+++ server/app.js
@@ -8,14 +8,15 @@ import views from 'koa-views';
 import serve from 'koa-static';
 
 import path from 'path';
+import axios from 'axios';
 
+import Geolocation from './geolocation';
 import exceptionGrabber from './exception-grabber';
 
-console.log(process.env.NODE_ENV);
 const root = process.env.NODE_ENV === 'production'
-                        ? __dirname
-                        : path.join(__dirname,'../');
-const static_files = path.join(root,'app-dist');
+    ? __dirname
+    : path.join(__dirname, '../');
+const static_files = path.join(root, 'app-dist');
 
 const app = new Koa();
 const router = Router();
@@ -23,7 +24,7 @@ app.use(exceptionGrabber)
     .use(BodyParser())
     .use(serve(static_files))
     .use(views(root, {
-        map:{
+        map: {
             html: 'underscore'
         }
     }))
@@ -39,6 +40,26 @@ app.use(exceptionGrabber)
 router.get('/', async (ctx, next) => {
     await ctx.render('index.html')
 });
+
+router.get('/user-latlong', async (ctx, next) => {
+    ctx.body = {
+        "lat": "-23.4733",
+        "long": "-46.6658",
+    }
+});
+
+router.get('/city-info', async (ctx, next) => {
+    let lat = ctx.query.latitude;
+    let long = ctx.query.longitude;
+    
+    let g = new Geolocation();
+    await g.getCityInfoByLatLong(lat,long).then( (data) => {
+        ctx.body = {name: data};
+    }).catch( (e) => console.log("deu erro",e))
+
+});
+
+
 
 app.listen(5050, () => console.log('Listening on port 5050.'));
 
