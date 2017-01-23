15ba6a80b4da98591d7d19b22dd63f1811d6d3d8
dansoah <fixmyprinter@danilorechi.net>
2017-01-23 02:57:50 -0200

    Add weather route

--- server/app.js
+++ server/app.js
@@ -11,7 +11,9 @@ import path from 'path';
 import axios from 'axios';
 
 import Geolocation from './geolocation';
+import Weather from './weather';
 import exceptionGrabber from './exception-grabber';
+import {kelvinToCelsius,kelvinToFahrenheit} from './convert';
 
 const root = process.env.NODE_ENV === 'production'
     ? __dirname
@@ -48,7 +50,6 @@ router.get('/user-latlong', async (ctx, next) => {
 
     let g = new Geolocation();
     await g.getLatLongByIp(ipv4).then( (data) => {
-        console.log(data);
         ctx.body = {
             "lat": data.latitude,
             "long": data.longitude,
@@ -65,6 +66,43 @@ router.get('/city-info', async (ctx, next) => {
     await g.getCityInfoByLatLong(lat,long).then( (data) => {
         ctx.body = {name: data};
     }).catch( (e) => console.log("deu erro",e))
+
+});
+
+router.get('/weather', async (ctx, next) => {
+    let lat = ctx.query.latitude;
+    let long = ctx.query.longitude;
+
+    const w = new Weather();
+
+    await w.getCurrentWeather(lat,long).then( (weather) => {
+        
+        ctx.body = {
+            temperature_c: kelvinToCelsius(weather.main.temp),
+            temperature_f: kelvinToFahrenheit(weather.main.temp),
+            humidity: weather.main.humidity,
+            min_c: kelvinToCelsius(weather.main.temp_min),
+            max_c: kelvinToCelsius(weather.main.temp_max),
+            min_f: kelvinToFahrenheit(weather.main.temp_min),
+            max_f: kelvinToFahrenheit(weather.main.temp_max),
+            sunrise_utc: weather.sys.sunrise,
+            sunset_utc: weather.sys.sunset,
+        }
+
+    }, () => {
+        ctx.body = {
+            temperature_c: 0,
+            temperature_f: 0,
+            humidity: 0,
+            min_c: 0,
+            max_c: 0,
+            min_f: 0,
+            max_f: 0,
+            sunrise_utc: 0,
+            sunset_utc: 0,
+        }
+    }).catch( (e) => console.log("deu erro",e))
+    
 
 });
 
