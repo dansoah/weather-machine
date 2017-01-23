29227dedf3efe2a924c608a84b6b7f991b38b6f3
dansoah <fixmyprinter@danilorechi.net>
2017-01-23 03:33:49 -0200

    add forecast

--- server/weather.js
+++ server/weather.js
@@ -8,8 +8,35 @@ export default class Weather{
 
     }
 
-    getCurrentWeather(latitude, logitude){  
+    getCurrentWeather(latitude, logitude){
         const url = 'http://api.openweathermap.org/data/2.5/weather?'
+                        +'lat='+latitude
+                        +'&lon='+logitude
+                        +'&APPID='+openWeatherMap.key;
+        
+        const g = new Geolocation();
+        
+        if (!g.latitudeIsValid(latitude))
+            return Promise.reject(
+                RangeError("latitude must be a number between -90 & 90"))
+        if (!g.longitudeIsValid(logitude))
+            return Promise.reject(
+                RangeError("longitude must be a number between -180 & 180"))
+        
+        return new Promise( (resolve, reject)=> {
+            axios.get(url).then( (response) => {
+                if(response.status === 200)
+                    return resolve(response.data);
+                return reject();
+            }, (err) => {
+                return reject(err);
+            })
+        })        
+
+    }
+
+    getForecast(latitude, logitude){
+        const url = 'http://api.openweathermap.org/data/2.5/forecast?'
                         +'lat='+latitude
                         +'&lon='+logitude
                         +'&APPID='+openWeatherMap.key;
--- server/weather.test.js
+++ server/weather.test.js
@@ -68,3 +68,60 @@ test('getCurrentWeather | Must reject when status is 401 ', t => {
     })
 
 })
+
+
+test('getForecast | Must resolve when status is 200 ', t => {
+
+    return new Promise((resolve, reject) => {
+
+        moxios.withMock(function () {
+
+            let onFulfilled = sinon.spy()
+            const w = new Weather();
+
+            w.getForecast(-16.92, 145.77).then(onFulfilled);
+
+            moxios.wait(function () {
+                let request = moxios.requests.mostRecent();
+                request.respondWith( {
+                    status:200,
+                    response:{a:"32"}
+                }).then( () => {
+                    resolve();
+                })
+            });
+        })
+
+    }).then( () => {
+        t.pass();
+    })
+
+})
+
+test('getForecast | Must reject when status is 401 ', t => {
+
+    return new Promise((resolve, reject) => {
+
+        moxios.withMock(function () {
+
+            let onFulfilled = sinon.spy()
+            const w = new Weather();
+
+            w.getForecast(-16.92, 145.77).then(onFulfilled);
+
+            moxios.wait(function () {
+                let request = moxios.requests.mostRecent();
+                request.respondWith( {
+                    status:401,
+                    response:{a:"32"}
+                }).then( reject, resolve)
+            });
+        })
+
+    }).then( () => {
+        t.fail();
+    }, () => {
+        t.pass();
+    })
+
+})
