c040a66875ec4991e0b295c28a86f9bf210d4524
dansoah <fixmyprinter@danilorechi.net>
2017-01-24 14:46:56 -0200

    Add forecast processing before displaying

--- app/js/weather/forecast/index.jsx
+++ app/js/weather/forecast/index.jsx
@@ -38,12 +38,60 @@ export default class Forecast extends React.Component {
     }
 
     getForecastInfo(latitude, longitude) {
+
+        const sum = (acm, val) => acm + val;
+
         return new Promise((resolve, reject) => {
             let url = '/forecast/?latitude=' + latitude
                 + '&longitude=' + longitude;
 
             axios.get(url).then((response) => {
-                resolve(response.data);
+                //todo: calculate day media
+                let result = response.data.map( (f) => {
+                    f.date = new Date(f.date * 1000);
+                    return f;
+                }).reduce( (list, f) => {
+                    let date = ("0" + f.date.getDate()).slice(-2) + "/" +
+                               ("0" + (f.date.getMonth() + 1)).slice(-2) + '/' +                               
+                                f.date.getFullYear();
+                               
+                    let selected = list.filter( (l) => l.date === date );
+
+                    if(selected.length === 0){
+                        list.push({ 'date':date,
+                                    'min_c':parseInt(f.min_c),
+                                    'max_c':parseInt(f.max_c),
+                                    'min_f':parseInt(f.min_f),
+                                    'max_f':parseInt(f.max_f)
+                                });
+                        return list;
+                    }
+
+                    if(f.min_c < selected[0].min_c)
+                        selected[0].min_c = parseInt(f.min_c);
+
+                    if(f.min_f < selected[0].min_f)
+                        selected[0].min_f = parseInt(f.min_f);
+
+                    if(f.max_c > selected[0].max_c)
+                        selected[0].max_c = parseInt(f.max_c);
+
+                    if(f.max_f > selected[0].max_f)
+                        selected[0].max_f = parseInt(f.max_f);
+
+                    return list;
+
+                }, [])
+                
+                let date = new Date();
+                let now = ("0" + date.getDate()).slice(-2) + "/" +
+                            ("0" + (date.getMonth() + 1)).slice(-2) + '/' +                               
+                            date.getFullYear();
+
+                if(now === result[0].date)
+                    result = result.slice(1);
+
+                resolve(result);
             })
         })
     }
@@ -51,16 +99,15 @@ export default class Forecast extends React.Component {
 
 
     render() {
-
+       
         let forecastList = this.state.forecast.map((f) => {
+            
             return <ForecastItem date={f.date}
-                temperatureC={f.temperature_c}
-                temperatureF={f.temperature_f}
-                minC={this.state.forecast.minC}
-                minF={this.state.forecast.minF}
-                maxC={this.state.forecast.maxC}
-                maxF={this.state.forecast.maxF}
-                humidity={this.state.forecast.maxF} 
+                minC={f.min_c}
+                minF={f.min_f}
+                maxC={f.max_c}
+                maxF={f.max_f}
+                humidity={f.humidity} 
                 isCelsius={this.props.isCelsius}/>
         })
 
